ad921135195dc4dae1d800ff3e717ea9f558a46f
Tristan Helmich <tristan.helmich@mayflower.de>
2014-05-24 11:00:53 +0200

    Use new repo (currently puppet 3.5.1)

--- packer/scripts/sles/puppet.sh
+++ packer/scripts/sles/puppet.sh
@@ -6,14 +6,13 @@ if [ -f /etc/zypp/locks ]; then
 fi
 if ! zypper lr -u | grep mayflower-puppet > /dev/null; then
   echo "[bootstrap] Add zypper home:mayflower:puppet repo" && \
-  #sudo zypper addrepo -f --no-gpgcheck http://download.opensuse.org/repositories/home:/mayflower:/puppet/SLE_11_SP3/ mayflower-puppet
-  sudo zypper addrepo -f --no-gpgcheck http://download.opensuse.org/repositories/systemsmanagement:/puppet/SLE_11_SP3/ puppet
+  sudo zypper addrepo -f --no-gpgcheck http://download.opensuse.org/repositories/home:/fadenb:/branches:/systemsmanagement:/puppet/SLE_11_SP3/ fadenb-puppet
 fi
 echo "[bootstrap] Installing git-core" && \
-  sudo zypper in -y --force-resolution git-core
-echo "[bootstrap] Installing current puppet version" && \
-  sudo zypper in -y --force-resolution puppet
+  sudo zypper --no-gpg-checks in -y --force-resolution git-core
+echo "[bootstrap] Installing puppet" && \
+  sudo zypper --no-gpg-checks in -y --force-resolution puppet
 echo "[bootstrap] Installing rubygems (prep for next step)" && \
-  sudo zypper in -y --force-resolution rubygems
+  sudo zypper --no-gpg-checks in -y --force-resolution rubygems
 echo "[bootstrap] Installing r10k, rgen and deep_merge for puppet" && \
   sudo gem install r10k rgen deep_merge
