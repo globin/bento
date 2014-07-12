a74cd714cb99e43c132365530ef6e999e74c9431
Tristan Helmich <tristan.helmich@mayflower.de>
2014-07-12 15:15:52 +0200

    WIP! set puppet to a non broken version

--- packer/scripts/common/puppet.sh
+++ packer/scripts/common/puppet.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# currently the only usable puppet version is 3.4.3-1
+# TODO: make puppet version configurable via ENV variable
+# this would allow setting the desired version in the packer template
+#PUPPETVERSION='=3.4.3-1puppetlabs1'
+PUPPETVERSION='=3.4.3-1'
+
 # Get codename in order to fetch correct .deb from Puppetlabs
 CODENAME=`lsb_release -s -c`
 
@@ -13,7 +19,8 @@ dpkg -i /tmp/puppetlabs-release-${CODENAME}.deb
 
 # update package lists (including new sources from .deb) + install puppet
 apt-get update
-apt-get install -y puppet facter
+apt-get install -y puppet${PUPPETVERSION} puppet-common${PUPPETVERSION} facter
+apt-get install -y libaugeas-ruby
 
 # enable puppet agent
 puppet agent --enable
