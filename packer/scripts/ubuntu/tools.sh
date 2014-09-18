56694d2801b98b881dbbcac5073acda827eb4fe9
Robin Gloster <robin@loc-com.de>
2014-09-18 14:36:43 +0200

    install build-essential

--- packer/scripts/ubuntu/tools.sh
+++ packer/scripts/ubuntu/tools.sh
@@ -3,4 +3,4 @@
 apt-get update
 
 # install some basic tools useful on nearly every dev vm
-apt-get install -y git patch vim-nox htop bash-completion wget curl
+apt-get install -y git patch vim-nox htop bash-completion wget curl build-essential
